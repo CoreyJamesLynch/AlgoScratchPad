7eee62955ad6e71b271ec2ea43100c94ac8c8cfc
bearinawolfpack <bearinawolfpack@gmail.com>
2021-02-22 20:05:20 -0500

    Complete initial HackerRank challenge

---
+++ SolveMeFirst/SolveMeFirst.js
@@ -0,0 +1,11 @@
+// OBJECTIVES:
+// Create a func names solveMeFirst
+// Pass intA and intB to solveMeFirst
+// Return sum of intA and intB
+
+// SOLUTION
+const solveMeFirst = (intA, intB) => intA + intB
+
+// TESTS
+console.log(solveMeFirst(2, 3))
+// -> 5
