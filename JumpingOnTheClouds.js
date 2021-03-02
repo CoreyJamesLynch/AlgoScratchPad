c72a0d7caa676b50ecf2b9c44186aa2d72ac77ec
bearinawolfpack <bearinawolfpack@gmail.com>
2021-03-01 20:52:01 -0500

    Finish problem explanation
    finished adding constraints
    finished adding example

--- JumpingOnTheClouds.js
+++ JumpingOnTheClouds.js
@@ -4,11 +4,21 @@
 -> The player can jump on any cloud having a number equal to the number of current clouds plus 1 or 2.
 -> The player must avoid all thunderheads.
 -> Each game, input will be arr of ints (clouds).
--> Clouds will be 0 (cumulus) or 1 (thunderhead)
+-> Clouds will be 0 (cumulus) or 1 (thunderhead).
+-> The number of each cloud will be it's index.
+-> The path taken will be represented by the indices of the clouds along the shortest path.
 
 OBJECTIVE
 -> Return the minimum number of jumps it will take to jump from the starting position to the last cloud.
 
 CONSTRAINTS
 -> It will always be possible to win the game.
+-> There will be no more than 100 clouds at a time.
+-> There will be no less than 2 clouds at a time.
+-> Each cloud can only be a 0 or 1.
+
+EXAMPLE
+clouds = [0, 1, 0, 0, 0, 1, 0]
+0 -> 2 -> 4 -> 6 (3) or 0 -> 2 -> 3 -> 4 -> 6 (4) 
+// -> Return 3
 */
