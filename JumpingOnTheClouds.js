f86207e3f6491c3b65f2ee95161022eb32034a13
bearinawolfpack <bearinawolfpack@gmail.com>
2021-03-01 20:45:14 -0500

    Add initial problem description to file

---
+++ JumpingOnTheClouds.js
@@ -0,0 +1,14 @@
+/* SUMMARY 
+-> Game starts with consecutively numbered clouds
+-> Clouds can be thunderheads or cumulus
+-> The player can jump on any cloud having a number equal to the number of current clouds plus 1 or 2.
+-> The player must avoid all thunderheads.
+-> Each game, input will be arr of ints (clouds).
+-> Clouds will be 0 (cumulus) or 1 (thunderhead)
+
+OBJECTIVE
+-> Return the minimum number of jumps it will take to jump from the starting position to the last cloud.
+
+CONSTRAINTS
+-> It will always be possible to win the game.
+*/
