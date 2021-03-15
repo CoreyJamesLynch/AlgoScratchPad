ab74afd50eaf149fdd1c002497d2644d291cd86b
bearinawolfpack <bearinawolfpack@gmail.com>
2021-03-15 13:47:54 -0400

    Add NarcissticNumberjs w/ examples

---
+++ NarcissisticNumber.js
@@ -0,0 +1,28 @@
+
+// Example
+// Input: 0
+// Output: 0 is an Armstrong number.
+// Explanation 0 = 0
+
+// Example
+// Input: 371
+// Output: 371 is an Armstrong number.
+// Explanation 3*3*3+7*7*7+1*1*1 = 371
+
+// Example
+// Input: 1634
+// Output: 1634 is an Armstrong number.
+// Explanation 1*1*1*1+6*6*6*6+3*3*3*3+4*4*4*4 = 1634
+
+// Example
+// Input: 321
+// Output: 321 is not an Armstrong number.
+// Explanation 3*3*3+2*2*2+1*1*1 = 44
+
+// Example
+// Input: 4321
+// Output: 4321 is not an Armstrong number.
+// Explanation 4*4*4*4+3*3*3*3+2*2*2*2+1*1*1*1 = 354
+
+
+
