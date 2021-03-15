76cbf5960efc5cc345581510b54d3536819038e5
bearinawolfpack <bearinawolfpack@gmail.com>
2021-03-15 14:43:43 -0400

    Create an array of ints from the digits of the input

--- NarcissisticNumber.js
+++ NarcissisticNumber.js
@@ -1,4 +1,3 @@
-
 // Example
 // Input: 0
 // Output: 0 is a narcissist number.
@@ -24,23 +23,32 @@
 // Output: 4321 is not a narcissist number.
 // Explanation 4*4*4*4+3*3*3*3+2*2*2*2+1*1*1*1 = 354
 
+const narcissistNumber = (int) => {
+  // split int into array of ints
+  const digitArray = Array.from(String(int), Number);
+  // iterate through array
+    // change each int of array to int to pow of array length
+      // find sum of new int array
+  // compare to int input and return conditional result
+  return digitArray;
+};
 
 //TESTS
 const start = new Date(); // Add tests below this line
 
-console.log(narcissistNumber(0))
+// console.log(narcissistNumber(0));
 // -> 0 is an narcissist number.
 
-console.log(narcissistNumber(371))
+// console.log(narcissistNumber(371));
 // -> 371 is an narcissist number.
 
-console.log(narcissistNumber(1634))
+console.log(narcissistNumber(1634));
 // -> 1634 is an narcissist number.
 
-console.log(narcissistNumber(321))
+// console.log(narcissistNumber(321));
 // -> 321 is not an narcissist number.
 
-console.log(narcissistNumber(4321))
+// console.log(narcissistNumber(4321));
 // -> 4321 is not an narcissist number.
 
 const end = new Date(); // Add tests above this line
