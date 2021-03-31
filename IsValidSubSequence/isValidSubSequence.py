3ef881807a98e198fc91a19ce190175c513f74c8
bearinawolfpack <bearinawolfpack@gmail.com>
2021-03-30 21:55:43 -0400

    Transpile isValisSubsequence from isValidSubsequencejs to isValidSubsequencepy

--- IsValidSubSequence/isValidSubSequence.py
+++ IsValidSubSequence/isValidSubSequence.py
@@ -1,25 +1,37 @@
 
 from time import process_time_ns
 
+# My Solution
+# def isValidSubsequence(firstList, secondList):
+#   if(len(firstList) < len(secondList)):
+#     return False
 
-def isValidSubsequence(firstList, secondList):
-  if(len(firstList) < len(secondList)):
-    return False
+#   firstListIdx = 0
+#   secondListIdx = 0
 
-  firstListIdx = 0
-  secondListIdx = 0
-  
-  while secondListIdx < len(secondList) and firstListIdx < len(firstList):
-      if firstList[firstListIdx] != secondList[secondListIdx]:
-          firstListIdx += 1
-      else:
-          secondListIdx += 1
-          firstListIdx += 1
-      
-  if secondListIdx >= len(secondList):
-      return True
+#   while secondListIdx < len(secondList) and firstListIdx < len(firstList):
+#       if firstList[firstListIdx] != secondList[secondListIdx]:
+#           firstListIdx += 1
+#       else:
+#           secondListIdx += 1
+#           firstListIdx += 1
 
-  return False
+#   if secondListIdx >= len(secondList):
+#       return True
+
+#   return False
+
+# Their Solution 1
+def isValidSubsequence(list, sequence):
+    listIdx = 0
+    seqIdx = 0
+
+    while listIdx < len(list) and seqIdx < len(sequence):
+        if list[listIdx] == sequence[seqIdx]:
+          seqIdx += 1
+        listIdx += 1
+
+    return seqIdx == len(sequence)
 
 
 # TESTS
@@ -28,10 +40,10 @@ start = process_time_ns()
 # print(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]))
 # -> true
 
-print(isValidSubsequence(
-    [5, 1, 22, 25, 6, -1, 8, 10],
-    [5, 1, 22, 22, 6, -1, 8, 10],
-)),
+# print(isValidSubsequence(
+#     [5, 1, 22, 25, 6, -1, 8, 10],
+#     [5, 1, 22, 22, 6, -1, 8, 10],
+# )),
 # -> false
 
 # print(
