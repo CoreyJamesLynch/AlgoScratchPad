055d5cf0889d1f207430040d70574e4be2439108
bearinawolfpack <bearinawolfpack@gmail.com>
2021-04-28 15:30:39 -0400

    Add Palindrome dir
    added solution1 with tests
    added readme with problem description

---
+++ Palindrome/README.md
@@ -0,0 +1,8 @@
+# Summary
+
+- Write a function that returns true if the input string is a palindrome and false if it is not.
+- A Palindrome is a word that is spelled the same forwards and backwards.
+
+## Constraints
+
+- Only non-empty strings consisting of lowercase english letters will be passed as an input.
---
+++ Palindrome/solution1.js
@@ -0,0 +1,46 @@
+const palindrome = (string) => {
+  return string;
+};
+
+// TESTS
+// testId: [input, expectedOutput]
+const tests = {
+  1: ['a', true],
+  2: ['aa', true],
+  3: ['ab', false],
+  4: ['aba', true],
+  5: ['abc', false],
+  6: ['redder', true],
+  7: ['renter', false],
+  8: ['racecar', true],
+  9: ['banana', false],
+};
+
+const startTest = () => {
+  const start = new Date();
+
+  testPalindrome(tests);
+
+  const end = new Date();
+  console.log(`Execution time ${end - start} ms`);
+};
+
+const testPalindrome = (tests) => {
+  for (test in tests) {
+    let testId = test;
+    let input = tests[test][0];
+    let expectedOutput = tests[test][1];
+
+    console.log(runTest(testId, input, expectedOutput));
+  }
+};
+
+const runTest = (testId, input, expectedOutput) => {
+  let actualOutput = palindrome(input);
+  let errorMsg = `Test ${testId} Failed. Expected ${actualOutput} to be ${expectedOutput}`;
+
+  if (actualOutput !== expectedOutput) return errorMsg;
+  return `Test ${testId} passed!`;
+};
+
+startTest();
