c23bd2a7f8a9a4c32c206c969b77d677b81528e9
bearinawolfpack <bearinawolfpack@gmail.com>
2021-05-12 12:59:28 -0400

    Add outOutRange func
    returns true if charCode is not in the english alphabet

--- scratchpad.js
+++ scratchpad.js
@@ -1,30 +1,20 @@
-// I want to create two objects with charCodes as IDs and their associated english letters as values.
-let englishUppercase = {};
-let englishLowercase = {};
-for (let idx = 1; idx <= 26; idx += 1) {
-  englishUppercase[64 + idx] = String.fromCharCode(64 + idx);
-  englishLowercase[96 + idx] = String.fromCharCode(96 + idx);
-}
-// One object for uppercase and One object for lowercase
-// console.log(englishUppercase);
-// console.log(englishLowercase);
+// A = 65
+// Z = 90
+// Shift = 1
+// Z shift 1 should output A
 
-// Then I want to check against those objects when deciding to shift
-let sampleStr = 'adFE 123 Pa';
-let shift = 1;
-// -> 'beGF 123 Qb'
-let shiftedStr = '';
-for (letter in sampleStr) {
-  let currentCharCode = sampleStr.charCodeAt(letter);
-  let currentChar = '';
-  // Then I can use those objects to determine what to shift to
-  if (
-    currentCharCode in englishLowercase ||
-    currentCharCode in englishUppercase
-  ) {
-    currentChar = String.fromCharCode(currentCharCode + (shift % 16));
-  } else currentChar = String.fromCharCode(currentCharCode);
-  shiftedStr += currentChar
-}
-console.log(shiftedStr);
+const outOfRange = (charCode) => {
+  return (charCode > 122 || (charCode > 90 && charCode < 96) || charCode < 64) 
+};
 
+const putInRange = (charCode, shift) => {
+  // break this code out into above/below upper and above/below lower
+  // if Uppercase(65 - 90), while(newCharCode + newShift > 90) newCharCode = 64 + ((newCharCode + newShift) - 90)
+  // test if charCode is upper or lower AND direction. Either cna be between after shift
+  if(charCode)
+}
+
+if (putInRange(90, 1)) console.log('CharCode is not in the alphabet');
+if (putInRange(64, -3)) console.log('CharCode is not in the alphabet');
+if (putInRange(122, 3)) console.log('CharCode is not in the alphabet');
+if (putInRange(122, 3)) console.log('CharCode is not in the alphabet');
