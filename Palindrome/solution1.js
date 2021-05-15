212ef2f06a04edef478091cad29fa1d571f26784
bearinawolfpack <bearinawolfpack@gmail.com>
2021-05-14 21:03:57 -0400

    Add palindrome
    first draft solution
    passes all unit tests

--- Palindrome/solution1.js
+++ Palindrome/solution1.js
@@ -1,5 +1,13 @@
 const palindrome = (string) => {
-  return string;
+  let startPointer = 0;
+  let endPointer = string.length - 1;
+  while(startPointer <= endPointer) {
+    if(string[startPointer] === string[endPointer]){
+      startPointer += 1
+      endPointer -= 1
+    } else return false
+  }
+  return true;
 };
 
 // TESTS
