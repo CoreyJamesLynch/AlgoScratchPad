b7d3e9a4327723ab532a7a2c43adda6bc6e9cf3d
bearinawolfpack <bearinawolfpack@gmail.com>
2022-11-22 16:10:39 -0500

    Refactor FindTwoThatSum and associated tests

--- Go/FindTwoThatSum/find_two_that_sum.go
+++ Go/FindTwoThatSum/find_two_that_sum.go
@@ -1,30 +1,31 @@
 package main
 
+// find each int pair in numbers that add to sum
 func FindTwoThatSum(numbers []int, sum int) (int, int) {
 	var sumIndices [][]int
-	var idxDiff int = len(numbers)
-	var res1, res2 int
-
-	// find two ints from numbers that add to sum then append vals as a slice to sumIndices
 	for s := 0; s < len(numbers); s += 1 {
 		for f := len(numbers) - 1; f > s; f -= 1 {
 			if numbers[s]+numbers[f] == sum {
 				sumIndices = append(sumIndices, []int{s, f})
 			}
 		}
-		// iterate values of sumIndices to find pair w/ smallest difference
-		for _, indice := range sumIndices {
-			var diff int = indice[1] - indice[0]
-			if diff < idxDiff {
-				idxDiff = diff
-				res1, res2 = indice[0], indice[1]
-			}
+	}
+	return findSmallestIndicesDiff(sumIndices, len(numbers))
+}
+
+// find indices w/ smallest difference or return -1, -1 if no indices
+func findSmallestIndicesDiff(sumIndices [][]int, length int) (int, int) {
+	var idxDiff int = length
+	var res1, res2 int
+	for _, s := range sumIndices {
+		var diff int = s[1] - s[0]
+		if diff < idxDiff {
+			idxDiff = diff
+			res1, res2 = s[0], s[1]
 		}
 	}
-	// return indice w/ least diff and that represents vals in numbers that add to sum
 	if len(sumIndices) > 0 {
 		return res1, res2
 	}
-	// else return value that represents no pair of vals in numbers add to sum
 	return -1, -1
 }
--- Go/FindTwoThatSum/find_two_that_sum_test.go
+++ Go/FindTwoThatSum/find_two_that_sum_test.go
@@ -5,64 +5,41 @@ import (
 	"testing"
 )
 
+// false tests for -1, -1 default return
 func TestFindTwoThatSum(t *testing.T) {
-	// pack parameters into a struct for unpacking later
 	tests := []struct {
-		// argument
-		numbers []int
-		// argument
-		sum int
-		// notes if sum exists in slice (-1, -1) should be returned if false
+		numbers       []int
+		sum           int
 		possible      bool
 		lowestIdxDiff int
 	}{
-		// remember -> []int{put int slice values here}
-		{[]int{1, 2, 3, 4}, 7, true, 1}, // absolute value
+		{[]int{1, 2, 3, 4}, 7, true, 1},
 		{[]int{0, -1, 1}, 0, true, 1},
-		{[]int{0, 1, 1}, 0, false, 0},                 // -1 - -1
-		{[]int{10, 1, 12, 3, 7, 2, 2, 1}, 4, true, 1}, // 5 - 6
+		{[]int{0, 1, 1}, 0, false, 0},
+		{[]int{10, 1, 12, 3, 7, 2, 2, 1}, 4, true, 1},
 	}
-	// we don't care about the idx. tc is value at idx. test is arg slice above
 	for _, tc := range tests {
-		// call test, run this func.
-		// store "<arg slice> with sum <arg sum>" and anon test func as parameters
+		// success msg
 		t.Run(fmt.Sprintf("%v with sum %v", tc.numbers, tc.sum), func(t *testing.T) {
-			// append is used to copy the numbers so that even if the
-			// FindTwoThatSum code alters the slice (which it shouldn't!)
-			// a solution like the following isn't accepted:
-			//
-			//   numbers[0] = 0
-			//   numbers[1] = sum
-			//   return 0, 1
-			//
-			// If this doesn't make sense to you, don't worry about it
-			// for now :)
+			// tests if numbers slice was modified by FindTwoThatSum
 			numbersCopy := append([]int{}, tc.numbers...)
-			// int returns from FindToThatSum are stored in i, j respectively
-			// FindToThatSum is being passed a copy of arg slices above
 			i, j := FindTwoThatSum(numbersCopy, tc.sum)
-			// calls intSlicesEqual func to test the slice copied from FindTwoThatSum params match slices in the tests being passed to test that func
 			if err := intSlicesEqual(numbersCopy, tc.numbers); err != nil {
-				// return err msg if slices don't match
 				t.Fatalf("FindTwoThatSum() altered the numbers list; %v", err)
 			}
-			// if not false
+			// tests -1, -1 is only returned when no values add to sum
 			if !tc.possible {
-				// and -1, -1 is returned
 				if i != -1 && j != -1 {
-					// return error msg. no sum should always be false and return -1, -1
 					t.Fatalf("FindTwoThatSum() = (%v, %v); want (%v, %v)", i, j, -1, -1)
 				}
-				// else return -1, -1
 				return
 			}
-			// sum return values of FindTwoThatSum
+			// returned v. expected; any indices /w vals that match sum will pass here
 			got := tc.numbers[i] + tc.numbers[j]
-			// is summed return does not match expected return. This allows for multiple solutions
 			if got != tc.sum {
-				// return err msg
 				t.Fatalf("FindTwoThatSum() = (%v, %v); sum = %v; want sum %v", i, j, got, tc.sum)
 			}
+			// tests for specific indices w/ least difference
 			idxDiff := j - i
 			if idxDiff != tc.lowestIdxDiff {
 				t.Fatalf("Returned %v as first val idx. Expected %v.", idxDiff, tc.lowestIdxDiff)
@@ -71,23 +48,16 @@ func TestFindTwoThatSum(t *testing.T) {
 	}
 }
 
-// passes numbers copy and numbers then returns err
+// functionality for testing if FindTwoThatSum modifies original numbers slice
 func intSlicesEqual(got, want []int) error {
-	// compares length of numbers copy and numbers
 	if len(got) != len(want) {
-		// if len !=, return error msg
 		return fmt.Errorf("len(got) = %v; len(want) = %v", len(got), len(want))
 	}
-	// for idx and idx val in numbers copy
 	for i, gv := range got {
-		// set wv = numbers idx val
 		wv := want[i]
-		// compares numbers copy idx val and numbers idx val
 		if gv != wv {
-			// if values are not equal return err
 			return fmt.Errorf("got[%v] = %v; want[%v] = %v", i, gv, i, wv)
 		}
 	}
-	// otherwise carry on
 	return nil
 }
